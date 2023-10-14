1a877f5152bc50257774859e35a90ab62be5cce7
= <salaslilia00@gmail.com>
2023-10-14 12:25:54 -0400

    added functionality to play and pause button and it works

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function toggleVideoStatus() {
 //update play/pause icon
 function updatePlayIcon() {
   if (video.paused) {
-    play.innerHTML = "<i class="fa fa-play fa-2x"></i>"
+    play.innerHTML = '<i class="fa fa-play"></i>';
   } else {
-    play.innerHTML = "<i class="fa fa-pause fa-2x"></i>"
+    play.innerHTML = '<i class="fa fa-pause"></i>';
   }
 }
 
