6035cb37610404e9bc41673c6bc6ac8b71c5ebfa
= <salaslilia00@gmail.com>
2023-10-14 12:10:10 -0400

    added functions

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-// const video = document.getElementById("video");
-// const play = document.getElementById("play");
-// const stop = document.getElementById("stop");
-// const progress = document.getElementById("progress");
-// const timestamp = document.getElementById("timestamp");
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
-// //event listeners
-// video.addEventListener("click", toggleVideoStatus);
-// video.addEventListener("pause", updatePlayIcon);
-// video.addEventListener("play", updatePlayIcon);
-// video.addEventListener("timeupdate", updateProgress);
+//Functions
 
-// play.addEventListener("click", toggleVideoStatus);
+//Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-// stop.addEventListener("click", stopVideo);
+//update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = "<i class="fa fa-play fa-2x"></i>"
+  } else {
+    play.innerHTML = "<i class="fa fa-pause fa-2x"></i>"
+  }
+}
 
-// progress.addEventListener("change", setVideoProgress);
+//Update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+//stop vide
+function stopVideo() {
+  return true;
+}
+
+//event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
